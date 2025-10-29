e515388dbf62551cc6605970ea41d5612ad11456
Matheus Santos <desenv.mathsantos@gmail.com>
2025-10-29 16:06:21 -0300

    Desafio_novato_concluído

--- xadrez.c
+++ xadrez.c
@@ -30,7 +30,7 @@ int main() {
 
     //Declaração de variáveis
 
-    int menu;
+    int opcao, i;
     char peca;
 
 
@@ -39,63 +39,96 @@ int main() {
     printf("    XADREZ: The Game     \n\n");
     printf("\n\n");
 
-    printf("1 - Novo Jogo\n");
-    printf("2 - Tutorial");
-    printf("3 - Sair do jogo");
-    scanf("%d", menu)
+
+    printf("         MENU         \n");
+    printf("   1 - Novo Jogo\n");
+    printf("   2 - Tutorial\n");
+    printf("   3 - Sair do jogo\n\n");
+    printf("   Escolha : ");
+    scanf("%d", &opcao);
 
     //Escolha entre opções do Menu
     //1 - Novo Jogo
     //2 - Tutorial
     //3 - Sair do Jogo
-    switch (menu)
+    switch (opcao)
     {
-    case 1:
-        //Escolha da peça
-        printf("Iniciando Novo Jogo\n\n\n");
-        printf(" Escolha qual peça mover: \n");
-        printf("T - Torre\n");
-        printf("B - Bispo\n");
-        printf("D - Dama / Rainha");
-        scanf("%c", &peca);
+        case 1:
+            //Escolha da peça
+            printf("\n\n\n");
+            printf("Iniciando Novo Jogo\n\n\n");
+            printf(" Escolha qual peça mover: \n");
+            printf("T - Torre\n");
+            printf("B - Bispo\n");
+            printf("D - Dama / Rainha\n\n");
+            printf(" Escolha: ");
+            scanf(" %c", &peca);
 
-        switch (peca)
-        {
-            //Move-se em linha reta horizontalmente ou verticalmente.
-            //Caso escolha Torre - programa deverá simular o movimento da Torre cinco casas para a direita.
-            case 'T':
-                for (int i = 0; i < 5; i++)
-                {
-                    printf("Direita\n"); // Indica a direção do movimento
-                }
-            break;
-            
-            case 'B':
-                //Move-se na diagonal.
-                //Caso escolha Bispo - Seu programa deverá simular o movimento do Bispo cinco casas na diagonal para cima e à direita.
-                //Para representar a diagonal, você imprimirá a combinação de duas direções a cada casa (ex: "Cima, Direita").
-                while ('')
-                {
-                    /* code */
-                }
+            switch (peca)
+            {
+                //Move-se em linha reta horizontalmente ou verticalmente.
+                //Caso escolha Torre - programa deverá simular o movimento da Torre cinco casas para a direita.
+                case 'T':
+                case 't':
+                    for (i = 0; i < 5; i++)
+                    {
+                        printf("Direita\n"); // Indica a direção do movimento
+                    }
+                    break;
                 
-            break;
+                case 'B':
+                case 'b':
+                    //Move-se na diagonal.
+                    //Caso escolha Bispo - Seu programa deverá simular o movimento do Bispo cinco casas na diagonal para cima e à direita.
+                    //Para representar a diagonal, você imprimirá a combinação de duas direções a cada casa (ex: "Cima, Direita").
+                    while (i < 5)
+                    {
+                        printf("Cima, Direita.\n");
+                        i++;
+                    }                
+                    break;
 
-            case 'D':
+                case 'D':
+                case 'd':
+                    i = 0;
+                    // Move-se em todas as direções.
+                    // Seu programa deverá simular o movimento da Rainha oito casas para a esquerda.
+                    do
+                    {
+                        printf("Esquerda\n");
+                        i++;
+                    } while (i <= 8);                
+                    break;
+            
+                default:
+                    printf ("Peça incorreta\n\n");
+                    printf(" Fim de Jogo!\n");
+                    break;
+            }
+            break;
+        case 2:
+            printf("\n\n\n");
+            printf("\n\n========== TUTORIAL ==========\n\n");
+            printf("1 - Escolha a peça que você queira movimentar.\n");
+            printf("   Abreviações das peças:\n");
+            printf("      R: Rei\n");
+            printf("      D: Dama (Rainha)\n");
+            printf("      T: Torre\n");
+            printf("      B: Bispo\n");
+            printf("      C: Cavalo\n");
+            printf("      P: Peão\n\n");
+            printf("2 - Cada peça se move de forma diferente.\n");
+            printf("3 - Neste jogo, você poderá testar seus movimentos.\n");
+            printf("==============================\n");       
             break;
         
+        case 3:
+            printf("Saindo do jogo.");
+            break;            
         default:
-            printf ("Peça incorreta\n\n")
+
+            printf("Escolha errada. ");
             break;
-        }
-
-
-
-
-        break;
-    
-    default:
-        break;
     }
 
 
