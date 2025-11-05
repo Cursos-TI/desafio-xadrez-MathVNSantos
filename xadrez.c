c56a302f4eae3962fe8a4222ef33414b55ff1428
Matheus Santos <desenv.mathsantos@gmail.com>
2025-11-05 12:49:57 -0300

    funções

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,44 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+//Função para mover a segunda torre do jogo
+//As funções serão utilizadas exclusivamente para aquelas peças que possem pares (Torre, Cavalo, Bispo)
+void movertorre (int casas){
+    if (casas > 0)
+    {
+        printf("Esquerda\n");
+        movertorre(casas - 1);
+    }
+    
+}
+void moverbispo1 (int casas){
+    if (casas > 0)
+    {
+        printf("Cima, Direita\n");
+        moverbispo1(casas - 1);
+    }
+}
+
+void moverbispo2 (int casas){
+    for (int vertical =  1; vertical <= casas; vertical++){
+        for (int horizontal = 1; horizontal <= 1; horizontal++){
+            printf("Cima, Esquerda\n");
+        }
+    }
+}
+
+void moverrainha (int casas){
+    if (casas > 0)
+    {
+        printf("Direita\n");
+        moverrainha(--casas);
+    }
+}
+
+void movercavalo (int casas){
+    
+}
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
@@ -58,39 +96,42 @@ int main() {
             printf("\n\n\n");
             printf("Iniciando Novo Jogo\n\n\n");
             printf(" Escolha qual peça mover: \n");
-            printf("T - Torre\n");
-            printf("B - Bispo\n");
+            printf("T / t - Torre da Esquerda ou Direita\n");
+            printf("B / b - Bispo da Esquerda ou Direita\n");
             printf("D - Dama (Rainha)\n");
-            printf("C - Cavalo\n");
+            printf("C / c - Cavalo da Esquerda ou Direita\n");
             printf(" Escolha: ");
-            scanf(" %c", &peca);
+            scanf(" %s", &peca);
 
             switch (peca)
             {
                 //Move-se em linha reta horizontalmente ou verticalmente.
                 //Caso escolha Torre - programa deverá simular o movimento da Torre cinco casas para a direita.
                 case 'T':
-                case 't':
                     for (i = 0; i < 5; i++)
                     {
                         printf("Direita\n"); // Indica a direção do movimento
                     }
                     break;
+
+                case 't':
+                    movertorre(5);
+                    break;                    
                 
                 case 'B':
-                case 'b':
                     //Move-se na diagonal.
                     //Caso escolha Bispo - Seu programa deverá simular o movimento do Bispo cinco casas na diagonal para cima e à direita.
                     //Para representar a diagonal, você imprimirá a combinação de duas direções a cada casa (ex: "Cima, Direita").
-                    while (i < 5)
-                    {
-                        printf("Cima, Direita.\n");
-                        i++;
-                    }                
+                    int ncasas = 5;
+                    moverbispo1(ncasas);              
                     break;
 
+                case 'b':
+                    moverbispo2(5);
+                    break;
+
+                //Movimentação da Rainha
                 case 'D':
-                case 'd':
                     i = 0;
                     // Move-se em todas as direções.
                     // Seu programa deverá simular o movimento da Rainha oito casas para a esquerda.
@@ -99,6 +140,9 @@ int main() {
                         printf("Esquerda\n");
                         i++;
                     } while (i <= 8);                
+                    break;
+                case 'd':
+                    moverrainha(8);
                     break;
                 
                 case 'C':
